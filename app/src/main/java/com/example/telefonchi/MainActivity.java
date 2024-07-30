7b793508754708d56051a50a7b863f6d72a1d100
hamzaqurbonov <77965943+hamzaqurbonov@users.noreply.github.com>
2024-07-30 15:15:58 +0500

    SearchView Dashboardga qo'shildi
    ishladi

--- app/src/main/java/com/example/telefonchi/MainActivity.java
+++ app/src/main/java/com/example/telefonchi/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.telefonchi.ui.dashboard.DashboardFragment;
+import com.example.telefonchi.ui.home.HomeFragment;
+import com.example.telefonchi.ui.notifications.NotificationsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +21,7 @@ import com.example.telefonchi.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    BottomNavigationView navView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +30,46 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView = findViewById(R.id.nav_view);
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        navView.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+
+
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+//        NavigationUI.setupWithNavController(binding.navView, navController);
     }
+
+
+    private  final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
+
+        Fragment selectedFragment = null;
+        int itemId = item.getItemId();
+        if (itemId == R.id.navigation_home) {
+            selectedFragment = new HomeFragment();
+
+        } else if (itemId == R.id.navigation_dashboard) {
+            selectedFragment = new DashboardFragment();
+
+        }else if (itemId == R.id.navigation_notifications) {
+            selectedFragment = new NotificationsFragment();
+
+        }
+
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, selectedFragment).commit();
+        }
+        return true;
+    };
+
+
+
 
 
 }
--- app/src/main/java/com/example/telefonchi/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/telefonchi/ui/dashboard/DashboardFragment.java
@@ -2,15 +2,24 @@ package com.example.telefonchi.ui.dashboard;
 
 import static java.util.Collections.*;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -38,12 +47,12 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.Flow;
 
 public class DashboardFragment extends Fragment {
     DashboardModel dashboardModel;
     public List<DashboardModel> activityList = new ArrayList<>();
-//    List<DashboardModel> asList = new ArrayList<>();
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference hadRef = db.collection("users/Yanvar/Yanvar");
 
@@ -51,17 +60,24 @@ public class DashboardFragment extends Fragment {
     private FragmentDashboardBinding binding;
     private RecyclerView recyclerView;
     private DashboardAdapter adapter;
+    private DashboardAdapter adapter2;
+    MenuItem menuItem;
+    SearchView searchView;
+    Toolbar toolbar;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
+//       binding = FragmentDashboardBinding.inflate(inflater, container, false);
+//        View root = binding.getRoot();
+        View view =    inflater.inflate(R.layout.fragment_dashboard, container, false);
 
-        recyclerView = root.findViewById(R.id.recycler_dashboard_Id);
+        recyclerView = view.findViewById(R.id.recycler_dashboard_Id);
 
-//        db.collection("grup").document("8FDD...").collection("anggota")
-//                .whereEqualTo("iduser", idUser)
-//                .orderBy("updatetime", Query.Direction.DESCENDING)
+        toolbar = view.findViewById( R.id.toolbar_search_dash);
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar );
+        activity.getSupportActionBar().setTitle("");
 
         db.collection("users").addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
@@ -119,76 +135,117 @@ public class DashboardFragment extends Fragment {
             }});
 
 
-
-//        List<DashboardModel>   мodalArrayList =
-//        List<DashboardModel> мodalArrayList =  prepareMemerList();
-
-        Log.d("demo28", "activityList "  + activityList);
-
-
-//        setUpRecyclerView();
-        return root;
+        return view;
     }
 
-
-//    private void setUpRecyclerView() {
-
-//        for (int i = 0; i<3; i++) {
-//            options = new FirestoreRecyclerOptions
-//                .Builder<DashboardModel>()
-//                .setQuery(db.collection("users/Yanvar/Yanvar").whereNotEqualTo("finishSum", 0), DashboardModel.class)
-//                .build();
-//            Log.d("demo29", "dashboardModel ");
-//        }
-
-//        options = new FirestoreRecyclerOptions
-//                .Builder<DashboardModel>()
-//                .setQuery(db.collection("users/Yanvar/Yanvar").whereNotEqualTo("finishSum", 0), DashboardModel.class)
-//                .build();
-
-
-//        Query query = hadRef.whereNotEqualTo("finishSum", 0);
-
-//        FirestoreRecyclerOptions<DashboardModel> options = new FirestoreRecyclerOptions.Builder<DashboardModel>().setQuery(query, DashboardModel.class).build();
-
-
-//        adapter = new DashboardAdapter(мodalArrayList);
-//
-//        recyclerView.setHasFixedSize(true);
-//        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),1  ));
-//        recyclerView.setAdapter(adapter);
-//    }
-
-    private List<DashboardModel> prepareMemerList() {
-
-        List<DashboardModel> modellist = new ArrayList<>();
-//        Map<String,Object> nestedData = new HashMap<>();
-//        nestedData.put("sum", "sdsd");
-//        nestedData.put("name", "sssss");
-
-        for (int i = 0; i<3; i++) {
-//            modellist.add(new DashboardModel("name " + i, "nick", 1, "comment", "year",2, 3,4, 5,6,7));
-
-//            Log.d("demo16", "asdd " + modellist);
-        }
-        return modellist;
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
     }
 
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu,@NonNull MenuInflater inflater) {
+//        getMenuInflater().inflate(R.menu.menu_search,menu);
 
 
+        inflater.inflate(R.menu.menu_search,menu);
+        menuItem = menu.findItem(R.id.menu_search_id);
+        searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        searchView.setIconified(true);
+        searchView.setQueryHint("Qidiruv");
 
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        adapter.startListening();
-//    }
+
+//        inflater.inflate(R.menu.menu_search,menu);
 //
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//        adapter.stopListening();
-//    }
+//        MenuItem item = menu.findItem(R.id.menu_search_id);
+//
+//        SearchView searchView = (SearchView)item.getActionView();
+////        searchView.setIconified(true);
+//        searchView.setQueryHint("Qidiruv");
+
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+//                processSearch(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                processSearch(s);
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    void processSearch(String s) {
+        activityList.clear();
+        db.collection("users").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.d("demo28", "Error : " + e.getMessage());
+                }
+                for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
+                    if (doc.getType() == DocumentChange.Type.ADDED) {
+                        Log.d("demo28", "users doc "  + doc.getDocument().getId());
+
+
+                        doc.getDocument().getReference().collection(doc.getDocument().getId()).whereNotEqualTo("finishSum", 0)
+                                .whereGreaterThanOrEqualTo("year", s)
+                                .whereLessThanOrEqualTo("year", s + "\uf8ff")
+                                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                            @Override
+                            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                                if (e != null) {
+                                    Log.d("demo28",  "collection1 "+ "Error : " + e.getMessage());
+                                } else {
+
+                                    for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
+                                        if (doc.getType() == DocumentChange.Type.ADDED) {
+//
+                                            Map<String, Object> values = doc.getDocument().getData();
+////
+                                            activityList.add(new DashboardModel(
+                                                    (String) values.get("name"),
+                                                    (String) values.get("nick"),
+                                                    (String) values.get("comment"),
+                                                    (String) values.get("year"),
+                                                    Integer.parseInt(values.get("totalSum").toString()),
+                                                    Integer.parseInt(values.get("startSum").toString()),
+                                                    Integer.parseInt(values.get("finishSum").toString()),
+                                                    Integer.parseInt(values.get("amountMonth").toString()),
+                                                    Integer.parseInt(values.get("sumMonth").toString()),
+                                                    Integer.parseInt(values.get("tel").toString()),
+                                                    Integer.parseInt(values.get("payment").toString())
+                                            ));
+
+//                                        DashboardModel dashboardModel = doc.getDocument().toObject(DashboardModel.class);
+//                                        Log.d("demo28", "collection2 "  + doc.getDocument().getId() + " " +  doc.getDocument().getData());
+//                                            Log.d("demo31", "collection3 " + doc.getDocument().getData().get("name"));
+
+//                                            Log.d("demo31", "collection4 " + activityList);
+                                            adapter = new DashboardAdapter(activityList);
+                                            recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1));
+                                            recyclerView.setAdapter(adapter);
+                                        }
+
+                                    }
+                                }
+
+                            }
+                        });
+                    }
+
+                }
+
+            }});
+    }
+
 
     @Override
     public void onDestroyView() {
